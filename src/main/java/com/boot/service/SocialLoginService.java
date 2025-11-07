e269768ac43848cc49802da31cfa8b43624d6c47
NamTaeYoung <150254921+NamTaeYoung@users.noreply.github.com>
2025-11-07 15:42:09 +0900

    Delete src directory

--- src/main/java/com/boot/service/SocialLoginService.java
+++
@@ -1,8 +0,0 @@
-package com.boot.service;
-
-import com.boot.dto.SocialUserDTO;
-
-public interface SocialLoginService {
-	public String getAccessToken(String code);
-	public SocialUserDTO getUserInfo(String accessToken);
-}
